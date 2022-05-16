67c68c21dee50c4cd620dfb354dbfaaba4f68dab
Alex Drapikovsky <drapikovskyan@gmail.com>
2022-05-17 01:08:27 +0300

    logger added

--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -1,6 +1,8 @@
 package taxi.service;
 
 import java.util.Optional;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import taxi.dao.DriverDao;
 import taxi.exception.AuthenticationException;
 import taxi.lib.Inject;
@@ -9,13 +11,16 @@ import taxi.model.Driver;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
     @Inject
     private DriverDao driverDao;
 
     @Override
     public Driver login(String login, String password) throws AuthenticationException {
+        logger.info("Login method was called. Incoming params login = {}", login);
         Optional<Driver> driver = driverDao.findByLogin(login);
         if (driver.isEmpty() && !driver.get().getPassword().equals(password)) {
+            logger.warn("Authentication was failed. Incoming params login = {};", login);
             throw new AuthenticationException("Login or password was incorrect");
         }
         return driver.get();
